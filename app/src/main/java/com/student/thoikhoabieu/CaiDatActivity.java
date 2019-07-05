7616720ece16db80a58f75d79feac4b8700b6c55
lebatrong <batrong2709@gmail.com>
2019-07-06 01:24:18 +0700

    xin quyen doc file

--- app/src/main/java/com/student/thoikhoabieu/CaiDatActivity.java
+++ app/src/main/java/com/student/thoikhoabieu/CaiDatActivity.java
@@ -1,13 +1,18 @@
 package com.student.thoikhoabieu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.util.Log;
@@ -28,12 +33,15 @@ public class CaiDatActivity extends AppCompatActivity {
     String filename = "Chọn âm thanh";
     String uriAmThanh = "";
 
+    private static final int REQUEST_ID_READ_PERMISSION = 100;
+    private static final int REQUEST_ID_WRITE_PERMISSION = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cai_dat);
 
+        initPermission();
         toolbar=(Toolbar)findViewById(R.id.toolbarcaidat);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -87,6 +95,39 @@ public class CaiDatActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else
+                Toast.makeText(this, "Permision Write File is Denied", Toast.LENGTH_SHORT).show();
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void initPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                //Permisson don't granted
+                if (shouldShowRequestPermissionRationale(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "Permission isn't granted ", Toast.LENGTH_SHORT).show();
+                }
+                // Permisson don't granted and dont show dialog again.
+                else {
+                    Toast.makeText(this, "Permisson don't granted and dont show dialog again ", Toast.LENGTH_SHORT).show();
+                }
+                //Register permission
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+            }
+        }
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         /*check whether you're working on correct request using requestCode , In this case 1*/
@@ -94,7 +135,6 @@ public class CaiDatActivity extends AppCompatActivity {
         if(requestCode == CODE && resultCode == Activity.RESULT_OK){
             Uri audioFileUri = data.getData();
             uriAmThanh = audioFileUri.toString();
-            Log.e("kiemtra", uriAmThanh);
             filename = getFileName(audioFileUri);
             btnChonAmThanh.setText(filename);
         }
